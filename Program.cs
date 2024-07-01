2bde01809d99f9e0577b5b2daaaff7432eb52eb0
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-07-02 00:39:55 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MessageBoxFunction
+{
+    public class Program
+    {
+        [DllImport("User32.dll", SetLastError = true)]
+        private static extern int MessageBox(IntPtr hWnd, string lpText, string lpCaption, IntPtr uType);
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetLastError();
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            MessageBox(IntPtr.Zero, "Pierwszy napis", "Tytuł", 0x00000004);
+            IntPtr error = GetLastError();
+            Console.WriteLine(error);
+        }
+    }
+}
